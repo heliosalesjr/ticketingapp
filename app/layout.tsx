a871d6a291c9282d2e88fdf8c9666ebbb1abf71d
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-08-12 18:38:23 -0300

    dark mode implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import MainNav from "@/components/MainNav";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,18 +19,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav className="flex flex-col items-center border-b mb-4 px-4 py-4">
-          <div className="max-w-5xl w-full">
-            <MainNav />
-          </div>
-        </nav>
-        <main className="flex flex-col items-center">
-          <div className="max-w-5xl w-full">
-            {children}
-          </div>
-        </main>
-        
-        </body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <nav className="flex flex-col items-center border-b mb-4 px-4 py-4">
+              <div className="max-w-5xl w-full">
+                <MainNav />
+              </div>
+            </nav>
+            <main className="flex flex-col items-center">
+              <div className="max-w-5xl w-full">
+                {children}
+              </div>
+            </main>
+          </ThemeProvider>  
+      </body>
     </html>
   );
 }
--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
+import {ModeToggle} from '@/components/ModeToggle'
 
 const MainNav = () => {
   return (
@@ -11,7 +12,10 @@ const MainNav = () => {
         </div>
         <div className='flex items-center gap-2'>
             <Link href="/">Logout</Link>
-            <Link href="/">Dark</Link>
+            <div className='px-4'>
+                <ModeToggle/>
+            </div>
+            
         </div>
     </div>
   )
