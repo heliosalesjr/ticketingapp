56664a9dbc741e3f15cd1b8cda3d34c926cbd414
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-08-19 18:08:42 -0300

    update ticket form

---
+++ app/api/tickets/[id]/route.ts
@@ -0,0 +1,55 @@
+import prisma from "@/prisma/db";
+import { ticketSchema } from "@/ValidationSchemas/ticket";
+import { NextRequest, NextResponse } from "next/server";
+
+interface Props {
+    params: { id: string }
+}
+
+
+export async function PATCH(request: NextRequest, { params }: Props) {
+    const body = await request.json();
+  
+    const validation = ticketSchema.safeParse(body);
+  
+    if (!validation.success) {
+      return NextResponse.json(validation.error.format(), { status: 400 });
+    }
+  
+    const ticket = await prisma.ticket.findUnique({
+      where: { id: parseInt(params.id) },
+    });
+  
+    if (!ticket) {
+      return NextResponse.json({ error: "Ticket Not Found" }, { status: 404 });
+    }
+  
+    if (body?.assignedToUserId) {
+      body.assignedToUserId = parseInt(body.assignedToUserId);
+    }
+  
+    const updateTicket = await prisma.ticket.update({
+      where: { id: ticket.id },
+      data: {
+        ...body,
+      },
+    });
+  
+    return NextResponse.json(updateTicket);
+  }
+  
+  export async function DELETE(request: NextRequest, { params }: Props) {
+    const ticket = await prisma.ticket.findUnique({
+      where: { id: parseInt(params.id) },
+    });
+  
+    if (!ticket) {
+      return NextResponse.json({ error: "Ticket Not Found" }, { status: 404 });
+    }
+  
+    await prisma.ticket.delete({
+      where: { id: ticket.id },
+    });
+  
+    return NextResponse.json({ message: "Ticket Deleted" });
+  }
