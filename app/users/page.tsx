e9b85d013037ca08288a3be8e69c5ed657e11e01
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-08-23 14:47:45 -0300

    users api

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,12 @@
+import UserForm from '@/components/UserForm'
 import React from 'react'
 
 const Users = () => {
   return (
-    <div>Users</div>
+    <div>
+      <UserForm />
+      
+    </div>
   )
 }
 
