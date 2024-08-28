08df8fb8bb100a88aee36cd8feebfc94d0a47d96
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-08-28 20:25:49 -0300

    assigning users

--- app/tickets/[id]/TicketDetail.tsx
+++ app/tickets/[id]/TicketDetail.tsx
@@ -1,87 +1,77 @@
-import React from 'react'
-import { Ticket } from '@prisma/client'
+import { Ticket, User } from "@prisma/client";
+import React from "react";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
-import TicketStatusBadge from '@/components/TicketStatusBadge';
-import TicketPriority from '@/components/ui/TicketPriority';
-import Link from 'next/link';
-import { buttonVariants } from '@/components/ui/button';
-
-import ReactMarkdown from 'react-markdown';
-import DeleteButton from './DeleteButton';
-  
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import TicketStatusBadge from "@/components/TicketStatusBadge";
+import TicketPriority from "@/components/TicketPriority";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import ReactMarkDown from "react-markdown";
+import DeleteButton from "./DeleteButton";
+import AssignTicket from "@/components/AssignTicket";
 
 interface Props {
-    ticket: Ticket;
-    
-  }
-const TicketDetail = ( {ticket}: Props) => {
-   return (
-    <div className='lg:grid lg:grid-cols-4'>
-        <Card className='mx-4 mb-4 lg:col-span-3 lg:mr-4'>
-            <CardHeader>
-                <div className='flex justify-between py-2 mb-4'>
-                    <TicketStatusBadge status={ticket.status} />
-                    <TicketPriority priority={ticket.priority} />
-                </div>
-                
-                <CardTitle>{ticket.title}</CardTitle>
-                
-            
-           
-                <CardDescription>Created: {ticket.createdAt.toLocaleDateString("en-US", {
-                year: "2-digit",
-                month: "2-digit",
-                day: "2-digit",
-                hour: "numeric",
-                minute: "2-digit",
-                hour12: true,
-                 })}
-                </CardDescription>
+  ticket: Ticket;
+  users: User[];
+}
 
-            </CardHeader>
-
-            <CardContent className='prose dark:prose-invert'>
-                <ReactMarkdown>
-                    {ticket.description}
-                </ReactMarkdown>
-                
-            </CardContent>
-            
-            <CardFooter>
-                Updated:{" "}
-                {ticket.updatedAt.toLocaleDateString("en-US", {
-                    year: "2-digit",
-                    month: "2-digit",
-                    day: "2-digit",
-                    hour: "numeric",
-                    minute: "2-digit",
-                    hour12: true,
-                })}
-            </CardFooter>
-        </Card>
-
-        <div className='mx-4 flex lg:flex-col lg:mx-0 gap-2'>
-            <Link 
-                href={`/tickets/edit/${ticket.id}`}
-                className={`${buttonVariants(
-                    {variant: 'default'}
-                )} mt-4`}
-            >
-                Edit Ticket
-            </Link>
-
-            <DeleteButton ticketId={ticket.id} />
-        </div>
+const TicketDetail = ({ ticket, users }: Props) => {
+  return (
+    <div className="lg:grid lg:grid-cols-4">
+      <Card className="mx-4 mb-4 lg:col-span-3 lg:mr-4">
+        <CardHeader>
+          <div className="flex justify-between mb-3">
+            <TicketStatusBadge status={ticket.status} />
+            <TicketPriority priority={ticket.priority} />
+          </div>
+          <CardTitle>{ticket.title}</CardTitle>
+          <CardDescription>
+            Created:{" "}
+            {ticket.createdAt.toLocaleDateString("en-US", {
+              year: "2-digit",
+              month: "2-digit",
+              day: "2-digit",
+              hour: "numeric",
+              minute: "2-digit",
+              hour12: true,
+            })}
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="prose dark:prose-invert">
+          <ReactMarkDown>{ticket.description}</ReactMarkDown>
+        </CardContent>
+        <CardFooter>
+          Updated:{" "}
+          {ticket.updatedAt.toLocaleDateString("en-US", {
+            year: "2-digit",
+            month: "2-digit",
+            day: "2-digit",
+            hour: "numeric",
+            minute: "2-digit",
+            hour12: true,
+          })}
+        </CardFooter>
+      </Card>
+      <div className="mx-4 flex lg:flex-col lg:mx-0 gap-2">
+        <AssignTicket ticket={ticket} users={users} />
+        <Link
+          href={`/tickets/edit/${ticket.id}`}
+          className={`${buttonVariants({
+            variant: "default",
+          })}`}
+        >
+          Edit Ticket
+        </Link>
+        <DeleteButton ticketId={ticket.id} />
+      </div>
     </div>
-   )
+  );
 };
 
-
-export default TicketDetail
+export default TicketDetail;
--- app/tickets/[id]/page.tsx
+++ app/tickets/[id]/page.tsx
@@ -1,21 +1,23 @@
-import prisma from '@/prisma/db'
-import React from 'react'
-import TicketDetail from './TicketDetail'
+import React from "react";
+import prisma from "@/prisma/db";
+import TicketDetail from "./TicketDetail";
 
 interface Props {
-  params: { id: string }
+  params: { id: string };
 }
 
 const ViewTicket = async ({ params }: Props) => {
-    const ticket = await prisma.ticket.findUnique({
-      where: { id: parseInt(params.id) },
-    });
-  
-    if (!ticket) {
-      return <p className=" text-destructive">Ticket Not Found...</p>;
-    }
-  
-    return <TicketDetail ticket={ticket} />;
-  };
-  
-  export default ViewTicket;
+  const ticket = await prisma.ticket.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  const users = await prisma.user.findMany();
+
+  if (!ticket) {
+    return <p className=" text-destructive">Ticket Not Found!</p>;
+  }
+
+  return <TicketDetail ticket={ticket} users={users} />;
+};
+
+export default ViewTicket;
