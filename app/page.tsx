b418e54cdfd99e1c40c3cf465f91abb5da533a48
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-09-01 18:19:07 -0300

    main dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,49 @@
-import Image from "next/image";
+import React from "react";
+import prisma from "@/prisma/db";
+import DashRecentTickets from "@/components/DashRecentTickets";
+import DashChart from "@/components/DashChart";
 
-export default function Home() {
+const Dashboard = async () => {
+  const tickets = await prisma.ticket.findMany({
+    where: {
+      NOT: [{ status: "CLOSED" }],
+    },
+    orderBy: {
+      updatedAt: "desc",
+    },
+    skip: 0,
+    take: 5,
+    include: {
+      assignedToUser: true,
+    },
+  });
+
+  const groupTicket = await prisma.ticket.groupBy({
+    by: ["status"],
+    _count: {
+      id: true,
+    },
+  });
+
+  const data = groupTicket.map((item) => {
+    return {
+      name: item.status,
+      total: item._count.id,
+    };
+  });
+
   return (
-    <>
-      <h1>BORA BIO</h1>
-    </>
+    <div>
+      <div className="grid gap-4 md:grid-cols-2 px-2">
+        <div>
+          <DashRecentTickets tickets={tickets} />
+        </div>
+        <div>
+          <DashChart data={data} />
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
+export default Dashboard;
